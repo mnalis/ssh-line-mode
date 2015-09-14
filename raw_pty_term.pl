407b73c3e52e207bcdde9ea3109be57e12eedb66
Matija Nalis <mnalis-git@voyager.hr>
2015-09-14 18:57:33 +0200

    added raw_pty_term.pl

---
+++ raw_pty_term.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w
+# demo: raw terminal using PTY; by Matija Nalis <mnalis-github@voyager.hr> GPLv3+, started 2015-09-14
+
+use IO::Pty::Easy;
+use Term::ReadKey;
+
+ReadMode 'cbreak';
+
+my $pty = IO::Pty::Easy->new ( raw => 0 );
+$pty->spawn("sh");
+
+while ($pty->is_active) {
+    my $output = $pty->read(0);
+    if (defined $output) {
+         print $output;
+         #print "[output ends]";
+         last if defined($output) && $output eq '';
+    }
+    
+    my $input = ReadKey(-1);
+    if (defined $input) {
+        #print "[got input]";
+        #print $input;
+        my $chars = $pty->write($input, 0);
+        last if defined($chars) && $chars == 0;
+    }
+}
+
+$pty->close;
+ReadMode 'restore';
+
